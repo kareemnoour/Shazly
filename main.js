6fc2d9a7298aae2c830a8b8bb1e8129447cef37f
kareemnoour <121294972+kareemnoour@users.noreply.github.com>
2025-02-04 02:21:14 +0200

    sec

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let dateTime = document.getElementById('dateTime');
+
 let upDateTime = () => {
     
     let date = new Date();
@@ -13,3 +14,31 @@ let upDateTime = () => {
 
 upDateTime();
 setInterval(upDateTime, 1000);
+
+let form = document.getElementById('form');
+let id = document.getElementById('id');
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let selectedDate = new Date(dateTime.value); 
+
+    let options = { day: "2-digit", month: "short", year: "numeric", hour: "2-digit", minute: "2-digit", hour12: true };
+    let formattedDate = selectedDate.toLocaleString("en-GB", options).replace(',', ' - ');
+    
+
+    let popup = document.querySelector('.popup');
+    popup.style.display = 'block';
+    popup.innerHTML = `
+            <h2>Thank you for submitting</h2>
+            <p>id:${id.value}</p>
+            <p>name:${name.value}</p>
+            <p>email:${email.value}</p>
+            <p>date& time:${formattedDate}</p>
+    `;
+    setTimeout(() => {
+        location.reload();
+    }, 3000);
+});
